2140ef71dda24d4f336000398a17f03220cd61b3
Vicente <carvalho.vicente0207@gmail.com>
2025-05-01 20:38:53 -0300

    Add intro base + header

--- nutribetes/src/App.jsx
+++ nutribetes/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
+import Logo from "./assets/logo.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <ul>
+          <li>INTRODUÇÃO</li>
+          <li>CONHEÇA</li>
+          <li>SOBRE</li>
+          <li>CONTATO</li>
+        </ul>
       </header>
 
       <section>
-        
+        <div className='intro'>
+          <h1>Texto de teste</h1>
+          <img src={Logo} alt="logo" className='logo1'/>
+        </div>
       </section>
       
     </div>
