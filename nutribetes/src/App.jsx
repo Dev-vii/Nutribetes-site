257d6f557438bce25ebc2f3dd10f995ef3c52445
Vicente <carvalho.vicente0207@gmail.com>
2025-05-02 10:36:37 -0300

    Add seção sobre

--- nutribetes/src/App.jsx
+++ nutribetes/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <header className="App-header">
         <ul>
           <li><a href="#intro" id='links'>INTRODUÇÃO</a></li>
-          <li>CONHEÇA</li>
-          <li>SOBRE</li>
+          <li><a href="#meet" id='links'>CONHEÇA</a></li>
+          <li><a href="#about" id='links'>SOBRE</a></li>
           <li>CONTATO</li>
         </ul>
       </header>
@@ -27,9 +27,41 @@ function App() {
             <h2>Nutricionista Claudineia Fiorini</h2>
             <p>Se você ou seu filho têm Diabetes Tipo 1, eu entendo os desafios diários que isso representa.</p>
             <p>A nutrição não é apenas sobre o que comemos – é sobre como nosso corpo<br /> responde aos alimentos. Por isso, meu acompanhamento é exclusivo e<br /> individualizado, ajudando você a encontrar Energia na Medida Certa.</p>
-            <img src={Foto1} alt="foto1" className='foto1'/>
+            <img src={Foto1} alt="foto1" className='foto1' />
             <div className='border1'></div>
           </div>
+        </div>
+
+        <div id='about'>
+          <h3>A Nutribetes cuida de você com estratégia!</h3>
+          <p>Cuidamos de você de forma integral</p>
+          <table>
+            <tr>
+              <div>
+                <h1>Diabetes <br />tipo 2</h1>
+              </div>
+              <div>
+                <h1>Hipertensão <br />arterial</h1>
+              </div>
+              <div>
+                <h1>Obesidade e sobrepeso</h1>
+              </div>
+              <div>
+                <h1>Doenças cardiovasculares</h1>
+              </div>
+            </tr>
+            <tr>
+              <div>
+                <h1>Colesterol <br />elevado</h1>
+              </div>
+              <div>
+                <h1>Síndrome <br />metabólica</h1>
+              </div>
+              <div>
+                <h1>Distúrbios da <br />tireoide</h1>
+              </div>
+            </tr>
+          </table>
         </div>
       </section>
 
