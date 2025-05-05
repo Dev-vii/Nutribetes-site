3f6023943bb247571299755786e7c7eae15101d0
Vicente <carvalho.vicente0207@gmail.com>
2025-05-04 21:17:22 -0300

    Cartões quase prontos

--- nutribetes/src/App.jsx
+++ nutribetes/src/App.jsx
@@ -3,10 +3,10 @@ import Logo from "./assets/logo.jpg"
 import Fruits from "./assets/fruits.jpg"
 import Foto1 from "./assets/foto1.JPG"
 
-import Diabetes2 from "./assets/diabetes.webp"
-import Hipertensão from "./assets/hipert.jpg"
-import Obesidade from "./assets/obesidade.jpg"
-import Cardio from "./assets/cardio.jpg"
+import Diabetes2 from "./assets/img/diabetes.webp"
+import Hipertensão from "./assets/img/hipert.jpg"
+import Obesidade from "./assets/img/obesidade.jpg"
+import Cardio from "./assets/img/cardio.jpg"
 
 
 // git@github.com:Dev-vii/Nutribetes-site.git = chave
@@ -47,30 +47,37 @@ function App() {
             <tr>
               <div id='Infocards'>
                 <h1>Diabetes <br />tipo 2</h1>
+                <p>Ajudamos você a<br /> equilibrar a glicemia<br /> com uma alimentação prática, saborosa e <br />possível.</p>
                 <img src={Diabetes2} alt="diabetes2" id='ImgBox' />
               </div>
               <div id='Infocards'>
                 <h1>Hipertensão <br />arterial</h1>
+                <p>Estratégias alimentares específicas para regular<br /> a pressão sem sacrifícios extremos.</p>
                 <img src={Hipertensão} alt="hipert" id='ImgBox' />
               </div>
               <div id='Infocards'>
                 <h1>Obesidade e sobrepeso</h1>
+                <p>Cuidado sem culpa, com foco em resultados<br /> reais e sustentáveis.</p>
                 <img src={Obesidade} alt="obesidade" id='ImgBox' />
               </div>
               <div id='Infocards'>
                 <h1>Doenças cardiovasculares</h1>
+                <p>Prevenção e controle<br /> com uma nutrição aliada<br /> à saúde cardíaca.</p>
                 <img src={Cardio} alt="cardio" id='ImgBox' />
               </div>
             </tr>
             <tr>
               <div id='Infocards'>
                 <h1>Colesterol <br />elevado</h1>
+                <p>Mudança de hábitos<br /> com foco na saúde do coração.</p>
               </div>
               <div id='Infocards'>
                 <h1>Síndrome <br />metabólica</h1>
+                <p>Um plano nutricional completo para restaurar<br /> o equilíbrio metabólico.</p>
               </div>
               <div id='Infocards'>
                 <h1>Distúrbios da <br />tireoide</h1>
+                <p>Suporte nutricional para modular o metabolismo, melhorar o humor e<br /> o bem-estar.</p>
               </div>
             </tr>
           </table>
